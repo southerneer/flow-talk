6cd8b0aa601830ffbd1591989e38275fe91b26ee
Eric Kirkham <eric@southerneer.com>
2016-12-06 23:01:02 -0800

    better 01

--- 01.js
+++ 01.js
@@ -2,8 +2,10 @@ function onlyWorksOnNumbers(foo, bar) {
   return foo * 10 + bar;
 }
 
-var doesThisWork = onlyWorksOnNumbers(10, 20)
-var howAboutThis = onlyWorksOnNumbers(10, '20')
+var doesThisWork = onlyWorksOnNumbers(10, 20);
+var howAboutThis = onlyWorksOnNumbers(10, '20');
+var andThis = onlyWorksOnNumbers('hello', 'there');
 
-console.log(doesThisWork)
-console.log(howAboutThis)
+console.log(doesThisWork);
+console.log(howAboutThis);
+console.log(andThis);
